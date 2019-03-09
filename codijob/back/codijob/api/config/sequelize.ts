e7cbd2ddf3cf11de170d624ae5b6e69e38299e67
Effren Anthony <effrenanthony2704@gmail.com>
2019-03-09 09:48:58 -0500

    Implementando sequelizer

---
+++ codijob/back/codijob/api/config/sequelize.ts
@@ -0,0 +1,29 @@
+// En este archivo ira la configuración 
+// e inicializacion, migracion de niestros modelos a la base de datos
+// udando el principio CODE FIRST
+
+import {skill_model} from '../models/skill'
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('codijob2','root','punkAnthony95',{
+    host: 'localhost',
+    dialect: 'mysql',
+    operatorAliases: false,
+    pool: {
+        max:5,
+        min:0,
+        acquire:30000,
+        idle:10000
+    } ,
+});
+
+export const Skill = skill_model(sequelize,Sequelize);
+// force => true; cada vez que el proyecto inicie (npm start)
+//          toda la dara y tablas se van a crear nuevamente sin datos.
+// force => false; solo va a crear las tablas y/o campos que bno figuren actualmente
+//          en nuetra base de datos, los datos no se van a borrar.
+sequelize.sync({force:true}).then(()=>{
+    console.log("Base de datos creada con exito");    
+});
+
+
--- codijob/back/codijob/index.ts
+++ codijob/back/codijob/index.ts
@@ -1,8 +1,6 @@
-// importando la ruta de home
-import { router_home } from './api/routes/home';
-// importando la ruta de empresas
-import { router_empresa } from './api/routes/empresa';
+// importando la ruta de skill
 import { router_skill } from './api/routes/skill';
+
 //Importamos la libreria desde node modules (require para importar la libreria en node_modules)
 var express = require('express'); 
 // app es una instancia directa de express
@@ -16,41 +14,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 // <--------------- / POST------------------>
 
-// realizando la conexion a MySql
-var mysql = require ('mysql');
-export var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'punkAnthony95',
-    database: 'codijob'
-});
-
-// global en node representa un objeto donde guardaremos variables que usaremos en cualquier 
-// archivo del proyecto
-
-// global.db = connection;
-
 // process.env.PORT es el valor que se le va a signar una vez encuetre el puerto de un servidor remoto
 const PUERTO = process.env.PORT || 3700;
 
 //importando las rutas
-// use => settea configuraciones de rutas, middlewares, cores, etc
-// use representa a la ruta padre de toda la api
-// ejemplo, si => app.use('/api',router_homre)
-// seria => localhost:3700/api/otraruta
-app.use('/api',router_home)
-app.use('/api',router_empresa)
+
 app.use('/api',router_skill)
+
 // listen es para levantar el servidor web (lanza o ejecuta el servidor web)
-// parametros => puerto, callback para indicar si se ejecutó correctamente
-app.listen(PUERTO,()=>{
-
-    connection.connect((error:any)=>{
-        if (error) {
-            throw(error);            
-        }
-        console.log("Conectado a la base de datos correctamente");        
-    });
-
+app.listen(PUERTO,()=>{ 
     console.log("Servidor corriendo perfectamente => localhost:3700");    
 });
