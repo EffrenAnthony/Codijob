3a35da305bd1da22c5bd542ad538e38d863ea211
Effren Anthony <effrenanthony2704@gmail.com>
2019-03-12 20:25:19 -0500

    Optimizando promesa en proyectoskill y proyectoskill Get

--- codijob/back/codijob/api/controllers/proyectoskill.ts
+++ codijob/back/codijob/api/controllers/proyectoskill.ts
@@ -4,38 +4,77 @@ import { ProyectoSkill} from '../config/sequelize';
 import { Skill} from '../config/sequelize';
 import { Proyecto} from '../config/sequelize';
 
-
+/**
+ * Forma 1 METODO GET
+ * ruta => /api/:parametro/ruta
+ * ejm => /api/1/ruta
+ * req.params.parametro
+ * 
+ * Forma 2 METODO GET
+ * ruta => /api/ruta
+ * ejm => /api/ruta?parametro = 8
+ * req.query.paramtro
+ */
 
 export var controller_proyectoSkill = {
     
     create: (req:Request, res:Response)=>{
         Skill.findById(req.body.skill_id).then((skill:any)=>{
-            Proyecto.findById(req.body.pro_id).then((proyecto:any)=>{
-                ProyectoSkill.create(req.body).then((proyectoSkillCreado:any)=>{
-                    let response = {
-                        message: "created",
-                        content: proyectoSkillCreado
-                    }
-                    res.status(201).send(response)
-                }).catch(()=>{   
-                })
-            }).catch(()=>{
+            if(skill != null){
+                return Proyecto.findById(req.body.pro_id);
+            }else{
                 let response = {
                     message: "error",
                     content: `El proyecto con id ${req.body.pro_id} no existe`
                 }
                 res.status(500).send(response);
-            })
-        }).catch(()=>{
-            let response = {
-                message: "error",
-                content: `El Skill con id ${req.body.skill_id} no existe`
+                throw ("Error => El skill no existe")
             }
-            res.status(500).send(response);
-        });
+        }).then((proyecto:any)=>{
+            if(proyecto != null){
+                return ProyectoSkill.create(req.body)
+            }else{
+                let response = {
+                    message: "error",
+                    content: `El skill existe pero el proyecto con id ${req.body.pro_id} no existe`
+                }
+                res.status(500).send(response);
+                throw ("ERROR => El proyecto no existe")
+            }   
+        }).then((nuevoproyectoskill:any)=>{
+            if(nuevoproyectoskill != null){
+                let response = {
+                    message: "created",
+                    content: nuevoproyectoskill
+                }
+                res.status(201).send(response)
+            }else{
+                let response = {
+                    message: "error",
+                    content: `Error al crear el proyectoskill`
+                }
+                res.status(418).send(response)
+                throw ("ERROR => El Proyecto Skill no existe")
+            }
+        }).catch((error:any)=>{
+            let response = {
+                message: "Error",
+                content: error
+            }
+            console.log(response);
+            
+        })
+    },
+
+    getSkillsByProyectoId: (req:Request, res:Response)=>{{
+        // params se usa cuando es get y body cuando se quiere enviar como formulario en un post
+       console.log(req.params.pro_id);
+       res.send("OK");
+       }
     }
 }
 
+            
 
 
 
@@ -89,9 +128,4 @@ export var controller_proyectoSkill = {
     // },
     // delete:(req:Request,res:Response)=>{     
     //     Proyecto.destroy()
-    // }
-
-
-    
-
-
+    // 
--- codijob/back/codijob/api/routes/proyectoskill.ts
+++ codijob/back/codijob/api/routes/proyectoskill.ts
@@ -5,7 +5,8 @@ export var router_proyectoSkill = Router();
 
 
 // router_proyectoSkill.get('/proyectoSkill/getAll',controller_proyectoSkill.getAll);
-router_proyectoSkill.post('/proyectoSkill/create',controller_proyectoSkill.create)
+router_proyectoSkill.post('/proyectoskill/create',controller_proyectoSkill.create)
+router_proyectoSkill.get('/proyectoskill/proyecto/:pro_id/skills',controller_proyectoSkill.getSkillsByProyectoId)
 // router_proyectoSkill.post('/proyectoSkill/update',controller_proyectoSkill.update)
 
 
